a8bc4766dbbd14f552c2ee3a0a1d91cb31490c74
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-27 15:37:18 +0100

    feat Create header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,66 @@
+<script>
+	import { gsap } from 'gsap';
+	import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+	import { onMount } from 'svelte';
+
+	gsap.registerPlugin(ScrollTrigger);
+
+	onMount(() => {
+		var headerTL = gsap.timeline();
+
+		headerTL
+			.from('.header-copy > *', {
+				autoAlpha: 0,
+				x: -1000,
+				stagger: 0.4,
+				ease: 'back.out(0.4)'
+			})
+			.from('.header-link', {
+				duration: 0.8,
+				autoAlpha: 0,
+				y: -600,
+				stagger: 0.05,
+				ease: 'back.out(1)'
+			});
+	});
+
+	const page_links = [
+		{ title: 'Home', href: '/' },
+		{ title: 'Resume', href: '/resume' },
+		{ title: 'Projects', href: '/projects' },
+		{ title: 'Contact Me', href: '/contact-me' }
+	];
+</script>
+
+<header class="container max-w-5xl">
+	<nav class="flex justify-between items-center py-5">
+		<div class="header-copy group">
+			<div class="overflow-hidden invisible">
+				<div
+					class="text-xl font-bold group-hover:-translate-y-[26px] transition-all ease-in-out h-[26px]"
+				>
+					<h1 class="text-red-500">Ibdul.</h1>
+					<h1>Ibrahim Abdulhameed.</h1>
+				</div>
+			</div>
+			<p class="page-title text-xs lowercase font-semibold opacity-50 invisible">About Me</p>
+		</div>
+		<ul class="inline-flex items-center justify-center">
+			{#each page_links as link}
+				<li class="header-link invisible">
+					<a
+						href={link.href}
+						class={`inline-block p-8 font-semibold text-sm transition-all ${
+							window.location.pathname == link.href
+								? 'text-red-500'
+								: 'hover:text-red-700 hover:pb-4'
+						}`}
+					>
+						{link.title}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<svelte:head>
+	<title>Ibrahim Abdulhameed</title>
+</svelte:head>
+
+<main class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20">
+	<Header />
+	<slot />
+</main>
