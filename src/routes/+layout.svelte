2f12ebc6b83307d6039c5c5e20adcd663e0d20e1
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-27 21:27:42 +0100

    feat make main slot flex-1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,8 @@
 	class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 min-h-screen flex flex-col justify-between "
 >
 	<Header />
-	<slot />
+	<div class="flex-1">
+		<slot />
+	</div>
 	<Footer />
 </main>
