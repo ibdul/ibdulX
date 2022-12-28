58eed3106ded8bc09455dd361c904604b383af01
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-28 18:58:57 +0100

    feat Make store and button for dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,30 +10,20 @@
 
 	export let data: PageData;
 
-	let dark_mode = false;
-
-	onMount(() => {
-		dark_mode = localStorage.getItem('dark_mode') == 'true';
-	});
-
-	function toggleDarkMode() {
-		dark_mode = !dark_mode;
-		localStorage.setItem('dark_mode', String(dark_mode));
-	}
+	import { dark_mode } from '../stores.js';
 </script>
 
-<div class:dark={dark_mode}>
+<div class:dark={$dark_mode}>
 	<div
-		class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 dark:from-[#16131F] dark:to-[#100E18] transition-all duration-300"
+		class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 dark:from-[#16131F] dark:to-[#100E18] transition-all duration-300 ease-in-out"
 	>
 		<PageTransition key={data.pathname} duration={500}>
-			<main class="min-h-screen flex flex-col justify-between text-defaults">
+			<main class="min-h-screen flex flex-col justify-between text-defaults svg-defaults">
 				<Header />
 				<div class="flex-1 flex">
 					<slot />
 				</div>
 				<Footer />
-				<p on:click={toggleDarkMode}>toggle dark mode</p>
 			</main>
 		</PageTransition>
 	</div>
