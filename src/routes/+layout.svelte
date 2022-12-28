3c354672126b70e914870400f7e5e4697a4afaa8
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-28 11:45:18 +0100

    feat Add dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,16 +8,22 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let dark_mode = true;
 </script>
 
-<div class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 ">
-	<PageTransition key={data.pathname} duration={500}>
-		<main class="min-h-screen flex flex-col justify-between text-defaults">
-			<Header />
-			<div class="flex-1">
-				<slot />
-			</div>
-			<Footer />
-		</main>
-	</PageTransition>
+<div class:dark={dark_mode}>
+	<div
+		class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 dark:from-[#16131F] dark:to-[#100E18] transition-all duration-300"
+	>
+		<PageTransition key={data.pathname} duration={500}>
+			<main class="min-h-screen flex flex-col justify-between text-defaults">
+				<Header />
+				<div class="flex-1 flex">
+					<slot />
+				</div>
+				<Footer />
+			</main>
+		</PageTransition>
+	</div>
 </div>
