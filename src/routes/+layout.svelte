bd068d7a7b2f81166a931593ba654a63efd3f969
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-28 02:20:57 +0100

    fix layout <head> overriding pages own

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,6 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<svelte:head>
-	<title>Ibrahim Abdulhameed</title>
-</svelte:head>
-
 <main
 	class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 min-h-screen flex flex-col justify-between text-defaults"
 >
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+	<title>Ibrahim Abdulhameed</title>
+</svelte:head>
