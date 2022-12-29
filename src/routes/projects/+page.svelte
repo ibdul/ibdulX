908c9ba52c67f02ee3a8e33a7a60f6e3f71d40f1
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2022-12-29 06:50:09 +0100

    feat Create projects page skeleton

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,4 +2,73 @@
 	<title>Projects | Ibrahim Abdulhameed</title>
 </svelte:head>
 
-<h1 class="text-2xl">Projects</h1>
+<section class="max-w-3xl container text-center space-y-8">
+	{#each [1, 2, 3, 4] as _}
+		<div class="space-y-4 overflow-clip relative border-primary border-b-2 !border-opacity-10">
+			<div class="space-y-4 ">
+				<h2 class="font-semibold">Hello</h2>
+				<div class="grid grid-cols-2 gap-4 items-start">
+					<div class="space-y-4">
+						<div class="w-full card min-h-[400px]" />
+						<div class="w-full card h-[300px]" />
+					</div>
+					<div class="space-y-4">
+						<div class="w-full card h-[300px]" />
+						<div class="w-full card min-h-[400px]" />
+					</div>
+				</div>
+			</div>
+			<div class="space-y-4 p-8 backdrop-blur-sm">
+				<p>
+					Du je des et bas verte les, et les d'enfants l'homme ces les ne les l'âcre, immobilités
+					vins des et qui aux - peaux. Jaune plus d'or plus plus suis soleil les m'ont. La aux
+					dispersant leur mer roulant colombes par. Fleuves suivi qu'une mers reculons sentant le
+					des fileur, les l'amour les fond les sentant perdu m'a. Traverser de de de figements a,
+					tout lames ou par avec savez-vous. Des sous taché lents niais cieux. Léger de pareille ô
+					aux les trouais j'ai ameres taché, géants et des poeme des quand une circulation des,
+					triques les clabaudeurs par eau le, parfois balottant dérades entonnoirs bleues la aux
+					doux je.
+				</p>
+				<p>
+					L'âcre et les les et d'enfants troupeaux. Épais des accroupi lointains d'or d'horreurs
+					qu'une longs. Regretter l'orgueil je un nus tout d'enfants qui voulu maelstroms. Plus du
+					soleil mes vers punaises les. Électriques langueurs enfants l'eau pourrit sans île revé
+					l'aube. Cotons acteurs les dont que n'ont, les béni sont que et je pensif aux le. Plus
+					ayant tout dévorés tremblais archipels la de, mer parapets figements avec ressacs lumineux
+					ô voulu gouffres taches, flache les fleurs et l'autre bords les ou liens. Cheveux a les
+					des pontons que couleurs subi, suivi ma les maritimes mes choient nos sillage, sans
+					mystiques les aux des l'orgueil de n'ont. Ainsi les vogueur c'est d'azur que pris ces des
+					et. Sur les hiver géants ouverts mes anses. Ma bateau sourd les que dans torpeurs ou doux.
+					Plus fleuves des d'argent pas et et le de carcasse.
+				</p>
+				<p>
+					Sur sont descendre golfes des sont de l'orgueil que lents. Haleurs verte des mystiques
+					plus mais, freles future des tu de des toute glaciers. Voir nus volets pénétra ou l'aube
+					bords ouverts ventouses des. Fleurs de des soleil des, les tout n'auraient l'âcre d'or et.
+					Juillets vomissures d'europe geindre et des nasses fleurs pareille m'a, joncs d'europe
+					monté navrantes aux, des des brumes lorsqu'a cloués de les de ces pussent. Je parfois
+					d'ombres de pareille j'aurais les de électriques des, géants la aux délirants pourrit
+					parfois t'exiles les et a. Et des entonnoirs un sentant désire la fientes enfants leur,
+					l'éveil embaumé l'aube vu brides les freles mes en, et loin mers ces et pontons les
+					d'oiseaux flots est-ce. Impassibles épais vu des n'auraient bleu parfois voguais, bercé
+					rougeoyant ô équipages flottaison les, navrantes montait fleuves dormir a poteaux a des
+					voulu, et j'ai les savez-vous des reculons.
+				</p>
+				<p>
+					Loin soleil les triques trouais quille sais colombes joncs. Nus - les les d'eau porteur
+					fileur cieux les dans, dérades des sous de aux taché plus et par hippocampes, rut ces
+					éclairs ô fleuves lents des béni sous mer, et aux leur reculons pontons. Lenteurs oiseau
+					bateau appelle de choient suivi ainsi. Des vos ultramarins blonds de l'assaut le, tout
+					tremblais par mer pareille frele soleil qu'un avec rougeoyant, et ne entonnoirs des
+					voulais ô vers sais poissons bons. Moi d'horreurs les m'a florides éternels. La horribles
+					de et du l'horizon marais bleu. Blés taché monté et.
+				</p>
+			</div>
+			<div
+				class="hidden absolute inset-x bottom-0 bg-gradient-to-b from-transparent via-white to-white shadow-md w-full -mt-2 p-8"
+			>
+				<p class="text-center font-semibold">expand</p>
+			</div>
+		</div>
+	{/each}
+</section>
