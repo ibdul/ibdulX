0d6aca1319b27cf9fd7b52fbf0662191ac9754d1
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-01-01 11:43:03 +0100

    feat Add page route progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
 	import '../app.css';
 
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	import '$lib/nprogress.css';
+	NProgress.configure({
+		parent: '.site_container',
+		minimum: 0.16
+	});
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
+
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
@@ -11,7 +28,7 @@
 	import { dark_mode } from '../stores.js';
 </script>
 
-<div class:dark={$dark_mode}>
+<div class="site_container" class:dark={$dark_mode}>
 	<div
 		class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 dark:from-[#16131F] dark:to-[#100E18] transition-all duration-300 ease-in-out overflow-clip"
 	>
