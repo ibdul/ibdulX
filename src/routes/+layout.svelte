061f9054eb7edc0361374bad524e57323e7cd487
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-01-01 01:36:36 +0100

    fix Remove pagetransitions it breaks the animations
    some scroll trigger animations break because of the pagetransitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,7 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
-	import PageTransition from '$lib/components/PageTransition.svelte';
 	import type { PageData } from './$types';
-	import { onMount } from 'svelte';
 
 	export let data: PageData;
 
@@ -17,14 +15,14 @@
 	<div
 		class="bg-gradient-to-br from from-slate-400/20 via-slate-200/20 dark:from-[#16131F] dark:to-[#100E18] transition-all duration-300 ease-in-out overflow-clip"
 	>
-		<PageTransition key={data.pathname} duration={500}>
-			<main class="min-h-screen flex flex-col justify-between text-defaults svg-defaults">
+		<main class="min-h-screen flex flex-col justify-between text-defaults svg-defaults">
+			{#key data.pathname}
 				<Header />
 				<div class="flex-1 flex">
 					<slot />
 				</div>
 				<Footer />
-			</main>
-		</PageTransition>
+			{/key}
+		</main>
 	</div>
 </div>
